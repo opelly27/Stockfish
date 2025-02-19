f65a195ca4ed6567199d466b41e1560a03920571
opelly27 <35671196+opelly27@users.noreply.github.com>
2025-02-19 10:18:39 -0800

    Update evaluate.h

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,8 +33,8 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-1c0000000000.nnue"
-#define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
+#define EvalFileDefaultNameBig "nn-287197aae49d.nnue"
+#define EvalFileDefaultNameSmall "nn-287197aae49d.nnue"
 
 namespace NNUE {
 struct Networks;
